5bb8eb6f56cdb5f4f847ba59d90837d979a3ab03
David Toth <david.toth@primarybid.com>
2022-12-04 10:29:10 +0000

    day4

---
+++ day4/solve.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import re
+
+with open("./input") as input:
+    Lines = input.read().splitlines()
+
+count = 0
+
+for line in Lines:
+    ranges = re.split(r'[,-]', line)
+    a = set(range(int(ranges[0]), int(ranges[1])+1))
+    b = set(range(int(ranges[2]), int(ranges[3])+1))
+    if a.issubset(b) or b.issubset(a):
+        count += 1
+print(count)
