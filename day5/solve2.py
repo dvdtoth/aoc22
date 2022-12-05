6d2e0f5273b483a21d8ae044f3caa4feeb14aaa1
David Toth <david.toth@primarybid.com>
2022-12-05 21:41:47 +0000

    day5 proud minimized spaghetti

--- day5/solve2.py
+++ day5/solve2.py
@@ -4,17 +4,9 @@ import string
 with open("./input") as input:
     Lines = input.read().splitlines()
 
-#[V] [B] [M] [M] [C] [Q] [C] [G] [H]
-    # first 8 is the map
-    # 9, 10 not needed
-    # instructions start at 11
-
-# move 5 from 4 to 7
-# move [repetitions] from [A] to [B]
-
-# Print the list
-
+# Map stack
 stack = {1:[],5:[],9:[],13:[],17:[],21:[],25:[],29:[],33:[]}
+key_index_list = dict(enumerate(stack.keys()))
 for num, line in enumerate(Lines):
     # Map initial layout
     if num < 8:
@@ -22,8 +14,6 @@ for num, line in enumerate(Lines):
             if k in [1,5,9,13,17,21,25,29,33] and c in string.ascii_uppercase:
                 stack[k].append(c)
     # Solve
-    key_index_list = dict(enumerate(stack.keys()))
-
     if num > 9:
         subs = line.split()
         iter, a, b = int(subs[1]), key_index_list[int(subs[3])-1], key_index_list[int(subs[5])-1]
@@ -31,6 +21,7 @@ for num, line in enumerate(Lines):
             if len(stack[a]) > 0:
                 stack[b].insert(0, stack[a][iter-1-i])
                 stack[a].pop(iter-1-i)
+# Print result
 for k,v in enumerate(stack):
     print(stack[v][0],end='')
 print("")
